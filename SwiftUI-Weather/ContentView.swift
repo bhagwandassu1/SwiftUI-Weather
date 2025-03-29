dc12b242b6c610fd42edae42c4ced7b65c3598ae
Bhagwandassu1 <bhagwandassu1@gmail.com>
2025-03-29 02:58:51 -0700

    Initial Commit

---
+++ SwiftUI-Weather/ContentView.swift
@@ -0,0 +1,25 @@
+//
+//  ContentView.swift
+//  SwiftUI-Weather
+//
+//  Created by Arashdeep Singh on 2025-03-29.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
+
